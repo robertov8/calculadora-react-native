d7ca649a58c92a623ac4f95b7af7b31197740a53
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-13 11:59:12 -0300

    Section 12, class 167

--- src/components/Operacao.js
+++ src/components/Operacao.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Picker, StyleSheet } from 'react-native';
 
-export default props => (
-    <Picker style={styles.operacao}>
-        <Picker.Item label='Soma' value='soma' />
-        <Picker.Item label='Subtração' value='subtracao' />
-    </Picker>
-);
+export default class Operacao extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { operacao: '' };
+    }
+
+    render() {
+        return (
+            <Picker
+                style={styles.operacao}
+                selectedValue={this.state.operacao}
+                onValueChange={op => this.setState({ operacao: op })}
+            >
+                <Picker.Item label='Soma' value='soma' />
+                <Picker.Item label='Subtração' value='subtracao' />
+            </Picker>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     operacao: {
