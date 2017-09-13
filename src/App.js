b773ad8b3139b48191f7bb36287c1a7fe77eb1a4
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-13 10:44:17 -0300

    Section 12, class 157

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import Topo from './components/Topo';
-import Resultado from './components/Resultado';
-import Painel from './components/Painel';
+import { Topo, Resultado, Painel } from './components';
+
 
 export default props => (
     <View>
